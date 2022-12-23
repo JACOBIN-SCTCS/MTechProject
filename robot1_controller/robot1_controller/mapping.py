f9781be0e1cb92405c2ec77855743d60d817942e
JACOBIN-SCTCS <jacobja3@gmail.com>
2022-12-23 17:01:00 +0000

    not working :(

--- robot1_controller/robot1_controller/mapping.py
+++ robot1_controller/robot1_controller/mapping.py
@@ -1,22 +1,24 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 import message_filters
+from message_filters import Subscriber
 
 
 class MapProcessor(Node):
     
     def __init__(self):
         super().__init__('robot_1_map_processor')
-        self.subscriber =  self.create_subscription(
-            Odometry,
-            'robot_1/odom',
-            self.odom_callback,
-            10
-        )
 
-    def odom_callback(self,msg):
-        self.get_logger().info(str(msg.pose.pose.position.x))
+        laser_sub = Subscriber('robot_1/laser/out',LaserScan)
+        odom_sub  = Subscriber('robot_1/odom',Odometry)
+
+        ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
+        ts.registerCallback(self.map_callback)
+
+    def map_callback(self,laser_scan,odom):
+        self.get_logger().info(str(odom.pose.pose.position.x))
     
 
 def main(args=None):
