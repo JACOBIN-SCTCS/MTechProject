8269a741aea0a8bd02386d1ccfb9b7feb826e509
JACOBIN-SCTCS <jacobja3@gmail.com>
2022-12-23 14:52:32 +0000

    Intermediate commit

---
+++ robot1_controller/launch/robot1.launch.py
@@ -0,0 +1,10 @@
+import os
+import pathlib
+import launch
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
+package_name = 'robot1_controller'
+
+def generate_launch_description():
+    pass
