cc031794a587e8dc2fe895be6cb8fed3acd7f990
JACOBIN-SCTCS <jacobja3@gmail.com>
2022-12-24 05:59:04 +0000

    Mapping part completed

--- robot1_controller/robot1_controller/mapping.py
+++ robot1_controller/robot1_controller/mapping.py
@@ -49,22 +49,126 @@ class MapProcessor(Node):
 
     def log_prob(self,p_x):
         return math.log(p_x / (1-p_x))
-    
+
+    def quarternion_to_euler(self,quat):
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
+
     def map_callback(self,laser_scan : LaserScan,odom : Odometry):
-        
-        self.get_logger().info(str(odom.pose.pose.position.x))
-        
+                
         MIN_ANGLE = laser_scan.angle_min
         ANGLE_INCREMENT = laser_scan.angle_increment
         current_angle = MIN_ANGLE
 
         try:
-            grid_x_position = 0
+            grid_x_position = int(self.CONVERSION_FACTOR*odom.pose.pose.position.x)
+            grid_y_poisition = int(self.CONVERSION_FACTOR*odom.pose.pose.position.y)
+            orientation = self.quarternion_to_euler(odom.pose.pose.orientation)
+        
+            for i in range(len(laser_scan.ranges)):
+                measurement = self.CONVERSION_FACTOR*min(laser_scan.range_max,laser_scan.ranges[i])
+                reflected_x_position = int(grid_x_position + measurement*math.cos(current_angle + orientation[2]))
+                reflected_y_position = int(grid_y_poisition + measurement*math.sin(current_angle + orientation[2]))
+                current_angle+= ANGLE_INCREMENT
+                self.bressenhams_line_drawing_algorithm((grid_x_position,grid_y_poisition),(reflected_x_position,reflected_y_position),measurement)
+                self.map_publisher.publish(self.occupancy_grid)
 
         except:
             print(" ")
         
+    # Reference Taken from a repository
+    def bressenhams_line_drawing_algorithm(self,coord0,coord1,sensor_measurement):
+        
+        x0 = coord0[0]
+        y0 = coord0[1]
 
+        x1 = coord1[0]
+        y1 = coord1[1]
+
+        dx = x1 - x0
+        dy = y1 - y0
+
+        is_steep = abs(dy) > abs(dx)
+
+        if is_steep:
+            x0,y0  = y0,x0
+            x1,y1 = y1,x1
+
+        swapped = False
+        if(x0 > x1):
+            x0 , x1 = x1 , x0 
+            y0 , y1 = y1 , y0
+            swapped = True
+        
+        dx = x1 - x0
+        dy = y1 - y0
+
+        error = int(dx / 2.0)
+        ystep = 1 if y0 < y1 else -1
+
+        y = y0
+        points = []
+        for x in range(x0,x1+1):
+            coord = (y,x) if is_steep else (x,y)
+            points.append(coord)
+            
+            error -= abs(dy)
+            if error < 0 :
+                y += ystep
+                error += dx
+        if swapped:
+            points.reverse()
+        
+        for i in range(len(points)-1):
+            self.log_map[(self.GRID_SIZE>>1)-1 + points[i][0]][(self.GRID_SIZE>>1)-1+ points[i][1]] += self.l_non_occ
+            
+            try:
+                probability_value  = 1 - 1/(1+math.exp(self.log_map[(self.GRID_SIZE>>1)-1 + points[i][0]][(self.GRID_SIZE>>1)-1+ points[i][1]]))
+            except OverflowError:
+                probability_value = float('-inf')
+
+
+            if(probability_value > 0.5):
+                self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[i][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[i][1])] = 100
+            else:
+                self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[i][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[i][1])] = 0
+        
+
+        if sensor_measurement < (self.CONVERSION_FACTOR*self.RANGE_MAX):
+            self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1 + points[len(points)-1][1]] += self.l_occ
+        else:
+            self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1 + points[len(points)-1][1]] += (self.l_prior)
+
+
+        try:
+            probability_value = max(float('-inf'), 1 - float(1/(1 + math.exp(self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1+ points[len(points)-1][1]]))))
+        except OverflowError:
+            probability_value = float('-inf')
+
+        if(probability_value >= 0.8):
+            self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[len(points)-1][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[len(points)-1][1])] = 100
+        else:
+            self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[len(points)-1][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[len(points)-1][1])] = 0
+
+        return points
 
 def main(args=None):
     rclpy.init(args=args)
