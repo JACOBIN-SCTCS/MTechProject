d30610453dd53bb7a8480ec6a4dd860e561d7aa3
JACOBIN-SCTCS <jacobja3@gmail.com>
2023-01-06 07:22:49 +0000

    added quarternion

--- robot1_controller/robot1_controller/robot_motion.py
+++ robot1_controller/robot1_controller/robot_motion.py
@@ -55,11 +55,33 @@ class MotionRobot1(Node):
         
         self.twist_publisher.publish(twist_message)'''
 
+
+    def quarternion_to_euler(self,quat):
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
     def odom_callback(self,msg : Odometry):
         
         twist_message = Twist()
         position = msg.pose.pose.position
-        orientation  = msg.pose.pose.orientation
+        orientation  = self.quarternion_to_euler(msg.pose.pose.orientation)
         destination = self.waypoints_to_follow[self.current_waypoint_index]
 
         inc_x = destination[0] - position.x
@@ -70,12 +92,12 @@ class MotionRobot1(Node):
             
 
         angle_to_goal = math.atan2(inc_y,inc_x)
-        if(abs(angle_to_goal - orientation.z) > self.absolute_difference_threshhold):
-            self.get_logger().info(str(abs(angle_to_goal - orientation.z)))
+        if(abs(angle_to_goal - orientation[2]) > self.absolute_difference_threshhold):
+            self.get_logger().info(str(abs(angle_to_goal - orientation[2])))
             twist_message.angular.z = -0.1
             twist_message.linear.x = 0.0
         else:
-            twist_message.linear.x = 0.3
+            twist_message.linear.x = 0.1
             twist_message.angular.z = 0.0
 
         self.twist_publisher.publish(twist_message)
