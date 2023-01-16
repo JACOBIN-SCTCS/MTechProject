1d29f505743a7e1b65db87ee9234b7c168471bbe
Jacob <jacobja3@gmail.com>
2023-01-16 12:33:06 +0530

    Did intermediate work

--- src/robot1_controller/robot1_controller/mapping.py
+++ src/robot1_controller/robot1_controller/mapping.py
@@ -5,63 +5,144 @@ from sensor_msgs.msg import LaserScan
 import message_filters
 from message_filters import Subscriber
 from rclpy.qos import QoSProfile,DurabilityPolicy,HistoryPolicy,ReliabilityPolicy
-from tf2_ros import StaticTransformBroadcaster,TransformStamped
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import StaticTransformBroadcaster,Buffer,TransformListener
 import math
 import numpy as np
+import threading
 
 
 class MapProcessor(Node):
     
-    def __init__(self):
-        super().__init__('robot_1_map_processor')
+    def __init__(self,robot_name='robot_1',world_size=20):
+        
+        self.robot_name = robot_name
+        self.robot_number = int(self.robot_name[6:])
+        super().__init__(self.robot_name+'_map_processor')
+        
+        self.RANGE_MAX = 1.0
 
-        self.GRID_SIZE = 20 * 100
+ 
+        self.RESOLUTION = 0.01
+        self.WORLD_SIZE = world_size
+        self.GRID_SIZE = self.WORLD_SIZE / self.RESOLUTION
+
+        # Initialize the probability values for the cell being an obstacle,
+        # free or belonging to the unknown region.
         self.NON_OCC_PROB = 0.2
         self.OCC_PROB = 0.8
         self.PRIOR_PROB = 0.5
-
-        self.CONVERSION_FACTOR = 50
-        self.RANGE_MAX = 1.0
-
+        
         self.l_occ = self.log_prob(self.OCC_PROB)
         self.l_prior = self.log_prob(self.PRIOR_PROB)
         self.l_non_occ = self.log_prob(self.NON_OCC_PROB)
-
-
-        laser_sub = Subscriber(self,LaserScan,'robot_1/laser/out')
-        odom_sub  = Subscriber(self,Odometry,'robot_1/odom')
-
-        #ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
-        ts = message_filters.ApproximateTimeSynchronizer([laser_sub,odom_sub],20,0.5)
-        ts.registerCallback(self.map_callback)
-
-        self.map_publisher = self.create_publisher(OccupancyGrid,'robot1_map',qos_profile=QoSProfile(
+        
+        self.log_map = np.array([[self.l_prior for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)])
+        self.map_publisher = self.create_publisher(OccupancyGrid,self.robot_name + '_map',qos_profile=QoSProfile(
                 depth=1,
                 durability=DurabilityPolicy.TRANSIENT_LOCAL,
                 reliability=ReliabilityPolicy.RELIABLE,
                 history=HistoryPolicy.KEEP_LAST,
             ))
+
+        self._map_lock = threading.Lock()
         
         self.tf_publisher = StaticTransformBroadcaster(self)
         tf = TransformStamped()
         tf.header.stamp = self.get_clock().now().to_msg()
         tf.header.frame_id = 'world_frame'
-        tf.child_frame_id = 'robot1_map'
+        tf.child_frame_id = self.robot_name+'_map'
         tf.transform.translation.x = 0.0
         tf.transform.translation.y = 0.0
         tf.transform.translation.z = 0.0
         self.tf_publisher.sendTransform(tf)
 
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer,self)
+        self.scanner_subscriber = self.create_subscription(LaserScan,self.robot_name+'/laser/out')
+        self.publish_map()
+        self.create_timer(1,self.publish_map)
+
+
+
+
+        self.CONVERSION_FACTOR = 50
+
+   
+
+
+
+        # NO NEED FOR APPROPIRATE TIME SYNCHRONIZER
+        laser_sub = Subscriber(self,LaserScan,'robot_1/laser/out')
+        odom_sub  = Subscriber(self,Odometry,'robot_1/odom')
+
+        #ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
+        ts = message_filters.ApproximateTimeSynchronizer([laser_sub,odom_sub],20,0.5)
+        ts.registerCallback(self.map_callback)        
+        # TO DELETE
         self.occupancy_grid = OccupancyGrid()
         self.occupancy_grid.info.width = self.GRID_SIZE
         self.occupancy_grid.info.height = self.GRID_SIZE
         self.occupancy_grid.info.resolution = 0.01
         self.occupancy_grid.header.frame_id = 'robot1_map'
         self.occupancy_grid.data = [-1 for i in range(self.GRID_SIZE*self.GRID_SIZE)]
-        self.log_map = np.array([[self.l_prior for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)])
+
+
 
     def log_prob(self,p_x):
-        return math.log(p_x / (1-p_x))
+        return np.log(p_x / (1.0-p_x))
+
+    def prob(self,log_prob):
+        result = np.exp(log_prob) / (1.0 + np.exp(log_prob))
+        if math.isnan(result):
+            result = 0.0
+        return result
+
+    def publish_map(self):
+        self._map_lock.acquire()
+        now = self.get_clock().now()
+        prob_map = [-1]*self.GRID_SIZE*self.GRID_SIZE
+        idx = 0
+
+        for i in range(self.GRID_SIZE):
+            for j in range(self.GRID_SIZE):
+                cell_prob= round(self.prob(self.log_map[i,j]),2)
+                prob_map[idx] = int(cell_prob*100)
+                idx+=1
+        msg = OccupancyGrid()
+        msg.header.stamp = now.to_msg()
+        msg.header.frame_id = self.robot_name+'_map'
+        msg.info.height = self.GRID_SIZE
+        msg.info.width =  self.GRID_SIZE
+        msg.info.origin.position.x = -(self.WORLD_SIZE/2.0)
+        msg.info.origin.position.y = -(self.WORLD_SIZE/2.0)
+        msg.data = prob_map
+        self.map_publisher.publish(msg)
+        self._map_lock.release()
+
+
+    def update_map(self, msg: LaserScan):
+        if self._map_lock.locked():
+            return
+        else:
+            self._map_lock.acquire()
+        
+        robot_rotation = None
+        robot_translation = None
+
+        try : 
+            transformation = self.tf_buffer.lookup_transform(target_frame='chassis'+str(self.robot_number),source_frame='odom',time=msg.header.stamp)
+            q = transformation.transform.rotation
+            robot_rotation = math.atan2(
+                                +2.0 * (q.w * q.z + q.x * q.y),
+                                +1.0 - 2.0 * (q.y * q.y + q.z * q.z)
+                            )
+            robot_translation = transformation.transform.translation
+        except:
+            self._map_lock.release()
+            return 
+
 
     def quarternion_to_euler(self,quat):
         x = quat.x
