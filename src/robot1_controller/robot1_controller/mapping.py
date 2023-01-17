d08408dead887ca69875bc1fea9b544349e972b4
Jacob <jacobja3@gmail.com>
2023-01-17 17:25:49 +0530

    added intermediate code for the mapping task

--- src/robot1_controller/robot1_controller/mapping.py
+++ src/robot1_controller/robot1_controller/mapping.py
@@ -80,8 +80,12 @@ class MapProcessor(Node):
                 reliability=ReliabilityPolicy.RELIABLE,
                 history=HistoryPolicy.KEEP_LAST,
             ))
-        
-        self.create_timer(2.0,self.publish_map)
+        self.publish_map()
+        self.scanner_subscriber = self.create_subscription(LaserScan,self.robot_name+'/laser/out',self.update_map,10)
+        self.scanner_subscriber
+
+
+        #self.create_timer(1,self.publish_map)
 
       
 
@@ -105,28 +109,52 @@ class MapProcessor(Node):
         self.map_publisher.publish(self.occupancy_grid)
 
 
+    def update_map(self, msg: LaserScan):
+        robot_rotation = None
+        robot_translation = None
 
-    def quarternion_to_euler(self,quat):
-        x = quat.x
-        y = quat.y
-        z = quat.z
-        w = quat.w
+        try:
+            transformation = self.tf_buffer.lookup_transform(target_frame='chassis'+str(self.robot_number),source_frame='odom',time=msg.header.stamp)
+            q = transformation.transform.rotation
+            robot_rotation = math.atan2(+2.0 * (q.w * q.z + q.x * q.y),+1.0 - 2.0 * (q.y * q.y + q.z * q.z))
+            robot_translation = transformation.transform.translation
+            #self.get_logger().info(str(robot_translation))
+        except:
+            return
         
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-     
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-     
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-     
-        return roll_x, pitch_y, yaw_z # in radians
+        lidar_coord_values = []
+        laser_range_angle = robot_rotation + msg.angle_min
+        for laser_range in msg.ranges:
+            if laser_range < msg.range_min or laser_range > msg.range_max:
+                continue
+            world_laser_x = robot_translation.x + laser_range * math.cos(laser_range_angle)
+            world_laser_y = robot_translation.y + laser_range * math.sin(laser_range_angle)
+            lidar_sensor_value = self.l_occ
+            if(laser_range == msg.range_max):
+                lidar_sensor_value = self.l_non_occ
+            lidar_coord_values.append((world_laser_x,world_laser_y,lidar_sensor_value))
+            laser_range_angle = laser_range_angle + msg.angle_increment
+        
+        robot_position_grid = (int(robot_translation.x/self.RESOLUTION), int(robot_translation.y/self.RESOLUTION))
+        for lidar_vals in lidar_coord_values:
+            laser_position_grid = (int(lidar_vals[0]/self.RESOLUTION),int(lidar_vals[1]/self.RESOLUTION))
+            points = self.bressenhams_algorithm(robot_position_grid,laser_position_grid)[:-1]
 
+            for i in range(len(points)):
+                self.log_map[(self.GRID_SIZE>>1) + points[i][0],(self.GRID_SIZE>>1) + points[i][1]] += (self.l_non_occ - self.l0)
+                cell_prob = self.actual_prob(self.log_map[(self.GRID_SIZE>>1) + points[i][0],(self.GRID_SIZE>>1) + points[i][1]])
+                cell_value = int(100*cell_prob)
+                self.occupancy_grid_data[(points[i][0]-int(self.WORLD_ORIGIN_X/self.RESOLUTION))*(int(self.GRID_SIZE)) + (points[i][1]-int(self.WORLD_ORIGIN_Y/self.RESOLUTION))] = cell_value
+            
+            self.log_map[(self.GRID_SIZE>>1) + laser_position_grid[0],(self.GRID_SIZE>>1) +  laser_position_grid[1]] += ( lidar_vals[2] - self.l0)
+            cell_prob = self.actual_prob(self.log_map[(self.GRID_SIZE>>1) + laser_position_grid[0],(self.GRID_SIZE>>1) + laser_position_grid[1]])
+            cell_value = int(100*cell_prob)
+            self.occupancy_grid_data[(laser_position_grid[0]-int(self.WORLD_ORIGIN_X/self.RESOLUTION))*(int(self.GRID_SIZE)) + (laser_position_grid[1]-int(self.WORLD_ORIGIN_Y/self.RESOLUTION))] = cell_value
+        
+        for i in range(self.GRID_SIZE):
+            self.occupancy_grid_data[i*self.GRID_SIZE + i] = 0
+        self.get_logger().info("new data has been added")   
+        self.publish_map()
 
     def map_callback(self,laser_scan : LaserScan,odom : Odometry):
                 
@@ -149,7 +177,43 @@ class MapProcessor(Node):
 
         except:
             print(" ")
-        
+
+
+    def bressenhams_algorithm(self,coord0,coord1):
+        x0 = coord0[0]
+        y0 = coord0[1]
+        x1 = coord1[0]
+        y1 = coord1[1]
+
+        dx = abs(x1-x0)
+        sx = 1 if x0 < x1 else -1
+        dy = -abs(y1-y0)
+        sy = 1 if y0 < y1 else -1
+
+        error = dx + dy
+
+        tmp_x = x0
+        tmp_y = y0
+        coordinate_points  = []
+        while True:
+            if(tmp_x==x1 and tmp_y == y1 ):
+                break
+            e2 = 2*error
+            if e2 >= dy:
+                if tmp_x==x1:
+                    break
+                error = error + dy
+                tmp_x = tmp_x + sx
+            if e2 <= dx:
+                if tmp_y == y1:
+                    break
+                error = error + dx
+                tmp_y = tmp_y + sy
+            coordinate_points.append((tmp_x,tmp_y))
+        return coordinate_points
+
+
+
     # Reference Taken from a repository
     def bressenhams_line_drawing_algorithm(self,coord0,coord1,sensor_measurement):
         
