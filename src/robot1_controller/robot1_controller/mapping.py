924442333fc3aa435daa04890af3c10049c0d38d
Jacob <jacobja3@gmail.com>
2023-01-18 13:55:20 +0530

    Starting from scratch

--- src/robot1_controller/robot1_controller/mapping.py
+++ src/robot1_controller/robot1_controller/mapping.py
@@ -4,75 +4,37 @@ from nav_msgs.msg import Odometry,OccupancyGrid
 from sensor_msgs.msg import LaserScan
 import message_filters
 from message_filters import Subscriber
-from rclpy.qos import QoSProfile,DurabilityPolicy,HistoryPolicy,ReliabilityPolicy
 from geometry_msgs.msg import TransformStamped
-from tf2_ros import StaticTransformBroadcaster,Buffer,TransformListener
+from rclpy.qos import QoSProfile,DurabilityPolicy,HistoryPolicy,ReliabilityPolicy
+from tf2_ros import StaticTransformBroadcaster
 import math
 import numpy as np
 
 
 class MapProcessor(Node):
     
-    def __init__(self,robot_name='robot_1',world_size=20):
-    
-        super().__init__(robot_name+'_map_processor')
-        self.robot_name = robot_name
-        self.robot_number = int(self.robot_name[6:])
+    def __init__(self):
+        super().__init__('robot_1_map_processor')
 
-        self.RESOLUTION = 0.01
-        self.WORLD_SIZE = world_size
-        self.GRID_SIZE = int(world_size/self.RESOLUTION) + 1
-        self.WORLD_ORIGIN_X = -(self.WORLD_SIZE/2.0)
-        self.WORLD_ORIGIN_Y = -(self.WORLD_SIZE/2.0)
-
-
-        self.NON_OCC_PROB = 0.8
-        self.OCC_PROB = 0.2
+        self.GRID_SIZE = 20 * 100
+        self.NON_OCC_PROB = 0.2
+        self.OCC_PROB = 0.8
         self.PRIOR_PROB = 0.5
-        self.P0 = 0.1
-
-
-        self.l_occ = self.log_prob(self.OCC_PROB)
-        self.l_prior = self.log_prob(self.PRIOR_PROB)
-        self.l_non_occ = self.log_prob(self.NON_OCC_PROB)
-        self.l0 = self.log_prob(self.P0)
-
 
         self.CONVERSION_FACTOR = 50
         self.RANGE_MAX = 1.0
 
-        self.tf_publisher = StaticTransformBroadcaster(self)
-        tf = TransformStamped()
-        tf.header.stamp = self.get_clock().now().to_msg()
-        tf.header.frame_id = 'world_frame'
-        tf.child_frame_id = self.robot_name+'_map'
-        tf.transform.translation.x = 0.0
-        tf.transform.translation.y = 0.0
-        tf.transform.translation.z = 0.0
-        self.tf_publisher.sendTransform(tf)
-        
-        
-        cell_prob = round(self.actual_prob(self.l_prior),2)
-        cell_value = int(100*cell_prob)
-        self.occupancy_grid_data = np.array([cell_value for i in range(self.GRID_SIZE*self.GRID_SIZE)])
+        self.l_occ = self.log_prob(self.OCC_PROB)
+        self.l_prior = self.log_prob(self.PRIOR_PROB)
+        self.l_non_occ = self.log_prob(self.NON_OCC_PROB)
 
-        now = self.get_clock().now()
-        self.occupancy_grid = OccupancyGrid()
-        self.occupancy_grid.header.stamp = now.to_msg()
-        self.occupancy_grid.info.width = self.GRID_SIZE
-        self.occupancy_grid.info.height = self.GRID_SIZE
-        self.occupancy_grid.info.resolution = self.RESOLUTION
-        self.occupancy_grid.header.frame_id = self.robot_name+'_map'
-        self.occupancy_grid.info.origin.position.x = self.WORLD_ORIGIN_X
-        self.occupancy_grid.info.origin.position.y = self.WORLD_ORIGIN_Y
-        self.occupancy_grid.data = self.occupancy_grid_data.tolist()
-        
 
-        self.log_map = np.array([[self.l_prior for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)])
+        laser_sub = Subscriber(self,LaserScan,'robot_1/laser/out')
+        odom_sub  = Subscriber(self,Odometry,'robot_1/odom')
 
-        self.tf_buffer = Buffer()
-        self.tf_listener =  TransformListener(self.tf_buffer,self)
-
+        #ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
+        ts = message_filters.ApproximateTimeSynchronizer([laser_sub,odom_sub],20,0.5)
+        ts.registerCallback(self.map_callback)
 
         self.map_publisher = self.create_publisher(OccupancyGrid,'robot1_map',qos_profile=QoSProfile(
                 depth=1,
@@ -80,89 +42,48 @@ class MapProcessor(Node):
                 reliability=ReliabilityPolicy.RELIABLE,
                 history=HistoryPolicy.KEEP_LAST,
             ))
-        self.publish_map()
-        self.scanner_subscriber = self.create_subscription(LaserScan,self.robot_name+'/laser/out',self.update_map,10)
-        self.scanner_subscriber
-
-
-        #self.create_timer(1,self.publish_map)
-
-      
-
-
         
+        self.tf_publisher = StaticTransformBroadcaster(self)
+        tf = TransformStamped()
+        tf.header.stamp = self.get_clock().now().to_msg()
+        tf.header.frame_id = 'world_frame'
+        tf.child_frame_id = 'robot1_map'
+        tf.transform.translation.x = 0.0
+        tf.transform.translation.y = 0.0
+        tf.transform.translation.z = 0.0
+        self.tf_publisher.sendTransform(tf)
+
+        self.occupancy_grid = OccupancyGrid()
+        self.occupancy_grid.info.width = self.GRID_SIZE
+        self.occupancy_grid.info.height = self.GRID_SIZE
+        self.occupancy_grid.info.resolution = 0.01
+        self.occupancy_grid.header.frame_id = 'robot1_map'
+        self.occupancy_grid.data = [-1 for i in range(self.GRID_SIZE*self.GRID_SIZE)]
+        self.log_map = np.array([[self.l_prior for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)])
 
     def log_prob(self,p_x):
         return math.log(p_x / (1-p_x))
 
-    def actual_prob(self,log_prob):
-        result = float(1.0 - (1.0/(1+np.exp(log_prob))))
-        if np.isnan(result):
-            result = 0.0
-        return result
-
-
-    def publish_map(self):
-        self.get_logger().info("Inside publish_map : About to publish map")
-        now = self.get_clock().now()
-        self.occupancy_grid.header.stamp = now.to_msg()
-        self.occupancy_grid.data = self.occupancy_grid_data.tolist()
-        self.map_publisher.publish(self.occupancy_grid)
-        self.get_logger().info("Inside publish_map : Published map")
-
-
-
-    def update_map(self, msg: LaserScan):
-        self.get_logger().info("Inside update_map : Going to update map")
-        robot_rotation = None
-        robot_translation = None
-        #self.get_logger().info("inside update map")
-        try:
-            transformation = self.tf_buffer.lookup_transform(target_frame='chassis'+str(self.robot_number),source_frame='odom',time=msg.header.stamp)
-            q = transformation.transform.rotation
-            robot_rotation = math.atan2(+2.0 * (q.w * q.z + q.x * q.y),+1.0 - 2.0 * (q.y * q.y + q.z * q.z))
-            robot_translation = transformation.transform.translation
-        except:
-            return
+    def quarternion_to_euler(self,quat):
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
         
-        
-        lidar_coord_values = []
-        laser_range_angle = robot_rotation + msg.angle_min
-        for laser_range in msg.ranges:
-            laser_range_val = min(laser_range,msg.range_max)
-            if laser_range_val < msg.range_min or laser_range_val > msg.range_max:
-                continue
-            world_laser_x = robot_translation.x + laser_range_val * math.cos(laser_range_angle)
-            world_laser_y = robot_translation.y + laser_range_val * math.sin(laser_range_angle)
-            #self.get_logger().info(str(world_laser_x)+","+str(world_laser_y))
-            lidar_sensor_value = self.l_occ
-            if(laser_range_val == msg.range_max):
-                lidar_sensor_value = self.l_non_occ
-            lidar_coord_values.append((world_laser_x,world_laser_y,lidar_sensor_value))
-            laser_range_angle = laser_range_angle + msg.angle_increment
-        
-        robot_position_grid = (int(robot_translation.x/self.RESOLUTION), int(robot_translation.y/self.RESOLUTION))
-        for lidar_vals in lidar_coord_values:
-            laser_position_grid = (int(lidar_vals[0]/self.RESOLUTION),int(lidar_vals[1]/self.RESOLUTION))
-            points = self.bressenhams_algorithm(robot_position_grid,laser_position_grid)[:-1]
-
-            for i in range(len(points)):
-                self.log_map[(self.GRID_SIZE>>1) + points[i][0],(self.GRID_SIZE>>1) + points[i][1]] += (self.l_non_occ - self.l0)
-                cell_prob = self.actual_prob(self.log_map[(self.GRID_SIZE>>1) + points[i][0],(self.GRID_SIZE>>1) + points[i][1]])
-                cell_value = int(100*(1-cell_prob))
-                self.occupancy_grid_data[(points[i][0]-int(self.WORLD_ORIGIN_X/self.RESOLUTION))*(int(self.GRID_SIZE)) + (points[i][1]-int(self.WORLD_ORIGIN_Y/self.RESOLUTION))] = cell_value
-            
-            self.log_map[(self.GRID_SIZE>>1) + laser_position_grid[0],(self.GRID_SIZE>>1) +  laser_position_grid[1]] += ( lidar_vals[2] - self.l0)
-            cell_prob = self.actual_prob(self.log_map[(self.GRID_SIZE>>1) + laser_position_grid[0],(self.GRID_SIZE>>1) + laser_position_grid[1]])
-            cell_value = int(100*(1-cell_prob))
-            self.occupancy_grid_data[(laser_position_grid[0]-int(self.WORLD_ORIGIN_X/self.RESOLUTION))*(int(self.GRID_SIZE)) + (laser_position_grid[1]-int(self.WORLD_ORIGIN_Y/self.RESOLUTION))] = cell_value
-        
-        for i in range(self.GRID_SIZE):
-            self.occupancy_grid_data[i*self.GRID_SIZE + i] = 0
-        #self.get_logger().info("new data has been added")   
-
-        self.publish_map()
-        self.get_logger().info("Inside update_map : Updated map")
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
 
 
     def map_callback(self,laser_scan : LaserScan,odom : Odometry):
@@ -186,43 +107,7 @@ class MapProcessor(Node):
 
         except:
             print(" ")
-
-
-    def bressenhams_algorithm(self,coord0,coord1):
-        x0 = coord0[0]
-        y0 = coord0[1]
-        x1 = coord1[0]
-        y1 = coord1[1]
-
-        dx = abs(x1-x0)
-        sx = 1 if x0 < x1 else -1
-        dy = -abs(y1-y0)
-        sy = 1 if y0 < y1 else -1
-
-        error = dx + dy
-
-        tmp_x = x0
-        tmp_y = y0
-        coordinate_points  = []
-        while True:
-            if(tmp_x==x1 and tmp_y == y1 ):
-                break
-            e2 = 2*error
-            if e2 >= dy:
-                if tmp_x==x1:
-                    break
-                error = error + dy
-                tmp_x = tmp_x + sx
-            if e2 <= dx:
-                if tmp_y == y1:
-                    break
-                error = error + dx
-                tmp_y = tmp_y + sy
-            coordinate_points.append((tmp_x,tmp_y))
-        return coordinate_points
-
-
-
+        
     # Reference Taken from a repository
     def bressenhams_line_drawing_algorithm(self,coord0,coord1,sensor_measurement):
         
