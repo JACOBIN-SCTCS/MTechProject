df39cfcb14bdc91bad6e2a1a110caf057ffe4e11
Jacob <jacobja3@gmail.com>
2023-01-18 17:52:33 +0530

    completed code for the mapping task with failure to adopt tf2 transforms

--- src/robot1_controller/launch/robot1.launch.py
+++ src/robot1_controller/launch/robot1.launch.py
@@ -3,16 +3,18 @@ import pathlib
 import launch
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+
 
 package_name = 'robot1_controller'
 
-MAX_ROBOTS = 1
+MAX_ROBOTS = 2
 robot_names = ['robot_'+str(i) for i in range(1,MAX_ROBOTS+1)]
 robot_world_sizes  = [20 for i in range(MAX_ROBOTS)]
 
 def generate_launch_description():
     
-    
+    use_sim_time = LaunchConfiguration('use_sim_time',default=True)
     robot_instances = [
         Node(package=package_name, executable='robot_1_map_processor',
                             arguments=[robot_names[i],str(robot_world_sizes[i])],
--- src/robot1_controller/robot1_controller/mapping.py
+++ src/robot1_controller/robot1_controller/mapping.py
@@ -6,11 +6,12 @@ import message_filters
 from message_filters import Subscriber
 from geometry_msgs.msg import TransformStamped
 from rclpy.qos import QoSProfile,DurabilityPolicy,HistoryPolicy,ReliabilityPolicy
-from tf2_ros import StaticTransformBroadcaster,Buffer,TransformListener
+from tf2_ros import StaticTransformBroadcaster,TransformListener, Buffer
+import tf2_ros
 import math
 import numpy as np
 import sys
-
+from rclpy.task import Future
 
 class MapProcessor(Node):
     
@@ -35,12 +36,12 @@ class MapProcessor(Node):
         self.l_non_occ = self.log_prob(self.NON_OCC_PROB)
 
 
-        #laser_sub = Subscriber(self,LaserScan,self.robot_name+'/laser/out')
-        #odom_sub  = Subscriber(self,Odometry,self.robot_name+'/odom')
+        laser_sub = Subscriber(self,LaserScan,self.robot_name+'/laser/out')
+        odom_sub  = Subscriber(self,Odometry,self.robot_name+'/odom')
 
-        #ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
-        #ts = message_filters.ApproximateTimeSynchronizer([laser_sub,odom_sub],20,0.5)
-        #ts.registerCallback(self.map_callback)
+        ts = message_filters.TimeSynchronizer([laser_sub,odom_sub],10)
+        ts = message_filters.ApproximateTimeSynchronizer([laser_sub,odom_sub],20,0.5)
+        ts.registerCallback(self.map_callback)
 
         self.map_publisher = self.create_publisher(OccupancyGrid,self.robot_name+'_map',qos_profile=QoSProfile(
                 depth=1,
@@ -71,9 +72,9 @@ class MapProcessor(Node):
         self.log_map = np.array([[self.l_prior for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)])
 
 
-        self.tf_buffer = Buffer()
-        self.tf_listener =  TransformListener(self.tf_buffer,self)
-        self.laser_subscriber = self.create_subscription(LaserScan,self.robot_name+'/laser/out',self.map_callback_no_odom,10)
+        #self.tf_buffer = Buffer()
+        #self.tf_listener =  TransformListener(self.tf_buffer,self)
+        #self.laser_subscriber = self.create_subscription(LaserScan,self.robot_name+'/laser/out',self.map_callback_no_odom,10)
 
     def log_prob(self,p_x):
         return math.log(p_x / (1.0-p_x))
@@ -84,44 +85,6 @@ class MapProcessor(Node):
         except:
             prob_value = 0.0 
         return prob_value
-
-    def map_callback_no_odom(self, msg : LaserScan):
-                
-        MIN_ANGLE = msg.angle_min
-        ANGLE_INCREMENT = msg.angle_increment
-        current_angle = MIN_ANGLE
-
-    
-        #self.get_logger().info("inside update map")
-    
-        try:
-            q = None
-            odom = None
-            try:
-                self.get_logger().info(str(msg.header.stamp))
-                transformation = self.tf_buffer.lookup_transform(target_frame='chassis'+str(self.robot_number),source_frame='odom',time=msg.header.stamp)
-                q = transformation.transform.rotation
-                odom = transformation.transform.translation
-            except:
-                self.get_logger().info("Failed to get the TF transform")
- 
-            grid_x_position = int(self.CONVERSION_FACTOR*odom.x)
-            grid_y_poisition = int(self.CONVERSION_FACTOR*odom.y)
-            q = odom.pose.pose.orientation
-            yaw = math.atan2(+2.0 * (q.w * q.z + q.x * q.y),+1.0 - 2.0 * (q.y * q.y + q.z * q.z))
-        
-            for i in range(len(msg.ranges)):
-                measurement = self.CONVERSION_FACTOR*min(msg.range_max,msg.ranges[i])
-                reflected_x_position = int(grid_x_position + measurement*math.cos(current_angle + yaw))
-                reflected_y_position = int(grid_y_poisition + measurement*math.sin(current_angle + yaw))
-                current_angle+= ANGLE_INCREMENT
-                self.bressenhams_line_drawing_algorithm((grid_x_position,grid_y_poisition),(reflected_x_position,reflected_y_position),measurement)
-                now = self.get_clock().now()
-                self.occupancy_grid.header.stamp = now.to_msg()
-                self.map_publisher.publish(self.occupancy_grid)
-
-        except:
-            self.get_logger().info("failed to parse")
 
     def map_callback(self,laser_scan : LaserScan,odom : Odometry):
                 
@@ -145,39 +108,33 @@ class MapProcessor(Node):
                 self.occupancy_grid.header.stamp = now.to_msg()
                 self.map_publisher.publish(self.occupancy_grid)
 
-        except:
-            print(" ")
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            self.get_logger().info(message)
         
-    # Reference Taken from a repository
     def bressenhams_line_drawing_algorithm(self,coord0,coord1,sensor_measurement):
         
         x0 = coord0[0]
         y0 = coord0[1]
-
         x1 = coord1[0]
         y1 = coord1[1]
-
         dx = x1 - x0
         dy = y1 - y0
 
         is_steep = abs(dy) > abs(dx)
-
         if is_steep:
             x0,y0  = y0,x0
             x1,y1 = y1,x1
-
         swapped = False
         if(x0 > x1):
             x0 , x1 = x1 , x0 
             y0 , y1 = y1 , y0
             swapped = True
-        
         dx = x1 - x0
         dy = y1 - y0
-
         error = int(dx / 2.0)
         ystep = 1 if y0 < y1 else -1
-
         y = y0
         points = []
         for x in range(x0,x1+1):
@@ -191,29 +148,31 @@ class MapProcessor(Node):
         if swapped:
             points.reverse()
         
+
+        last_point = ((self.GRID_SIZE>>1)-1 + points[len(points)-1][0],(self.GRID_SIZE>>1)-1 + points[len(points)-1][1])
+        last_point_flattened = ((self.GRID_SIZE>>1)-1 + points[len(points)-1][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[len(points)-1][1])
         for i in range(len(points)-1):
-            self.log_map[(self.GRID_SIZE>>1)-1 + points[i][0]][(self.GRID_SIZE>>1)-1+ points[i][1]] += self.l_non_occ
+            current_point = ((self.GRID_SIZE>>1)-1 + points[i][0] , (self.GRID_SIZE>>1)-1+ points[i][1] )
+            flattened_point = ((self.GRID_SIZE>>1)-1 + points[i][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[i][1]) 
+            self.log_map[current_point] += self.l_non_occ
         
-            probability_value  = self.prob_value(self.log_map[(self.GRID_SIZE>>1)-1 + points[i][0]][(self.GRID_SIZE>>1)-1+ points[i][1]])
-        
+            probability_value  = self.prob_value(self.log_map[current_point])
             if(probability_value > 0.5):
-                self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[i][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[i][1])] = 100
+                self.occupancy_grid.data[flattened_point] = 100
             else:
-                self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[i][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[i][1])] = 0
+                self.occupancy_grid.data[flattened_point] = 0
         
-
         if sensor_measurement < (self.CONVERSION_FACTOR*self.RANGE_MAX):
-            self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1 + points[len(points)-1][1]] += self.l_occ
+            self.log_map[last_point] += self.l_occ
         else:
-            self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1 + points[len(points)-1][1]] += (self.l_prior)
+            self.log_map[last_point] += (self.l_prior)
 
-        probability_value = max(float('-inf'), self.prob_value(self.log_map[(self.GRID_SIZE>>1)-1 + points[len(points)-1][0]][(self.GRID_SIZE>>1)-1+ points[len(points)-1][1]]))
+        probability_value = max(float('-inf'), self.prob_value(self.log_map[last_point]))
         if(probability_value >= 0.8):
-            self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[len(points)-1][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[len(points)-1][1])] = 100
+            self.occupancy_grid.data[last_point_flattened] = 100
         else:
-            self.occupancy_grid.data[((self.GRID_SIZE>>1)-1 + points[len(points)-1][0])*self.GRID_SIZE + ((self.GRID_SIZE>>1) -1 + points[len(points)-1][1])] = 0
+            self.occupancy_grid.data[last_point_flattened] = 0
 
-        #return points
 
 def main():
     args = sys.argv[1:]
