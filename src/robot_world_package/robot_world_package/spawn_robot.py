2938682bcf3d8eebd0f63bfbf714008078003982
Jacob <jacobja3@gmail.com>
2023-01-30 16:16:44 +0530

    updated robot world to print correct tf transforms

--- src/robot_world_package/robot_world_package/spawn_robot.py
+++ src/robot_world_package/robot_world_package/spawn_robot.py
@@ -5,6 +5,62 @@ from ament_index_python.packages import get_package_share_directory
 from gazebo_msgs.srv import SpawnEntity
 import xml.etree.ElementTree as ET
 
+def generate_custom_sdf(robot_number,sdf_tree):
+
+    new_chassis_name = 'chassis_' + str(robot_number)
+    
+    new_right_wheel_name = 'right_wheel_' + str(robot_number)
+    new_right_wheel_hinge_name = 'right_wheel_hinge_'+str(robot_number)  
+
+    new_left_wheel_name = 'left_wheel_' + str(robot_number)
+    new_left_wheel_hinge_name = 'left_wheel_hinge_'+str(robot_number)  
+    
+    new_laser_name = 'laser_' + str(robot_number)
+    new_laser_link_name = 'laser_link_' + str(robot_number)
+    new_laser_joint_name = 'laser_joint_' + str(robot_number)
+
+    new_ultra_name = 'ultrasonic_' + str(robot_number)
+    new_ultra_link_name = 'ultrasonic_link_' + str(robot_number)
+    new_ultra_joint_name = 'chassis2joint_' + str(robot_number)
+
+    sdf_tree[0][0].set('name',new_chassis_name)
+    sdf_tree[0][1].set('name',new_right_wheel_name)
+    sdf_tree[0][2].set('name',new_left_wheel_name)
+    
+    sdf_tree[0][3].set('name',new_left_wheel_hinge_name)
+    sdf_tree[0][3][2].text = new_chassis_name
+    sdf_tree[0][3][1].text = new_left_wheel_name
+   
+    sdf_tree[0][4].set('name',new_right_wheel_hinge_name)
+    sdf_tree[0][4][2].text = new_chassis_name
+    sdf_tree[0][4][1].text = new_right_wheel_name
+
+    sdf_tree[0][5].set('name',new_laser_link_name)
+    sdf_tree[0][5][-1].set('name',new_laser_name)
+
+    sdf_tree[0][6].set('name',new_laser_joint_name)
+    sdf_tree[0][6][0].text = new_laser_link_name
+    sdf_tree[0][6][1].text = new_chassis_name
+
+  
+    sdf_tree[0][7].set('name',new_ultra_link_name)
+    sdf_tree[0][7][0].set('name',new_ultra_name)
+
+    sdf_tree[0][8].set('name',new_ultra_joint_name)
+    sdf_tree[0][8][0].text = new_ultra_link_name
+    sdf_tree[0][8][1].text = new_chassis_name
+
+
+    sdf_tree[0][9][1].text = new_left_wheel_hinge_name
+    sdf_tree[0][9][2].text = new_right_wheel_hinge_name
+
+    sdf_tree[0][-1][-1].text = new_chassis_name
+    sdf_tree[0][-1][0][0].text = '/robot_'+str(robot_number)
+    sdf_tree[0][-3][-1][-1][0][0].text = '/robot_'+str(robot_number)
+    sdf_tree[0][-5][-1][-1][0][0].text = '/robot_'+str(robot_number)
+    new_sdf_text = ET.tostring(sdf_tree,encoding='unicode',method="xml")   
+
+    return new_sdf_text
 
 def main():
     argv = sys.argv[1:]
@@ -16,11 +72,7 @@ def main():
 
     sdf_text = open(sdf_file_path,'r').read()
     sdf_tree = ET.fromstring(sdf_text)
-    sdf_tree[0][-1][-1].text = 'chassis'+str(robot_number)
-    sdf_tree[0][-1][0][0].text = '/robot'+str(robot_number)
-    sdf_tree[0][-3][-1][-1][0][0].text = '/robot'+str(robot_number)
-    sdf_tree[0][-5][-1][-1][0][0].text = '/robot'+str(robot_number)
-    new_sdf_text = ET.tostring(sdf_tree,encoding='unicode',method="xml")   
+    new_sdf_text = generate_custom_sdf(robot_number,sdf_tree)
 
     node = rclpy.create_node("entity_spawner")
     client = node.create_client(SpawnEntity, "/spawn_entity")
