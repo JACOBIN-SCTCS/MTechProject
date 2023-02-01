c9c3d43af8d48413cd79f20adb1d65119d5c74db
JACOBIN-SCTCS <jacobja3@gmail.com>
2023-02-02 00:07:29 +0530

    added laser scan and rviz visualization

--- src/robot_world_package/launch/gazebo.launch.py
+++ src/robot_world_package/launch/gazebo.launch.py
@@ -2,10 +2,11 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration,Command
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
+import xacro
  
 package_name = 'robot_world_package'
 world_file_name = 'maze.world'
@@ -14,6 +15,13 @@ def generate_launch_description():
     
     use_sim_time = LaunchConfiguration('use_sim_time',default=True)
     pkg_dir = get_package_share_directory(package_name)
+    
+    sdf_file_path = os.path.join(
+        get_package_share_directory("robot_world_package"), "models",
+        "my_robot", "model.urdf")
+    
+    #sdf_text = xacro.process_file(sdf_file_path)
+
 
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir,'models')
     
@@ -26,6 +34,33 @@ def generate_launch_description():
             output='screen')
     
     robot_coordinates = [[0.0,0.0,0.0]]
+
+    robot_state_publishers = [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name = 'robot_state_publisher',
+            namespace='robot_' + str(i+1),
+            parameters=[{'frame_prefix': 'robot_'+str(i+1)+'/','use_sim_time':True, 'robot_description': Command(['xacro ', sdf_file_path]) }],
+            output='screen'
+        )
+
+        for i in range(len(robot_coordinates))
+    ]
+
+    joint_state_publishers = [
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            namespace='robot_' + str(i+1),
+            parameters=[{'frame_prefix': 'robot_'+str(i+1)+'/','use_sim_time':True, 'robot_description': Command(['xacro ', sdf_file_path]) }],
+            output='screen'
+        )
+        for i in range(len(robot_coordinates))
+    ]
+    
+
     robot_instances = [ 
                         Node(package=package_name, executable='spawn_robot',
                             arguments=['WarehouseBot'+str(i+1), 'robot_'+str(i+1), str(robot_coordinates[i][0]), str(robot_coordinates[i][1]), str(robot_coordinates[i][2])],
@@ -33,6 +68,6 @@ def generate_launch_description():
                         
                         for i in range(len(robot_coordinates))
                     ]
-    nodes = [gazebo] + robot_instances
+    nodes = [gazebo] + robot_state_publishers+ joint_state_publishers + robot_instances 
     
     return LaunchDescription(nodes)
