bc5f161552fcfb00d4b14de82fedd6a65054f120
Jacob <jacobja3@gmail.com>
2023-02-09 15:40:51 +0530

    addde sample code

--- src/robot_controller/robot_controller/robot_h_signature.py
+++ src/robot_controller/robot_controller/robot_h_signature.py
@@ -2,6 +2,9 @@ import rclpy
 from rclpy.node import Node
 from message_filters import Subscriber
 from nav_msgs.msg import OccupancyGrid
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+import numpy as np
 
 class TopologicalExplore(Node):
 
@@ -10,6 +13,10 @@ class TopologicalExplore(Node):
         self.navigating = False
 
         self.robot_name = robot_name
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer,self)
+        self.count = 0
+
         #map = Subscriber(self,OccupancyGrid,'/global_costmap/costmap')
         self.map_subscriber = self.create_subscription(OccupancyGrid,
             '/global_costmap/costmap',
@@ -19,11 +26,25 @@ class TopologicalExplore(Node):
         self.map_subscriber
 
     def map_callback(self,msg:OccupancyGrid):
-        if self.navigating:
-            return
-
+        #if self.navigating:
+        #    return
+        try:
+            t  = self.tf_buffer.lookup_transform('robot_1/base_footprint','map',rclpy.time.Time())
+            self.get_logger().info(str(t.transform.translation.x) + "," + str(t.transform.translation.y))
+        except Exception as ex:
+            self.get_logger().info('Could not find transform')
         self.navigating = True
         self.get_logger().info("Logging" + str(msg.header.frame_id))
+        if self.count == 0 :
+            f = open("/home/depressedcoder/sample.txt","w")
+            np_array = np.array(msg.data)
+            np_array = np.reshape(np_array,(-1,msg.info.width))
+            self.get_logger().info(str(np.any(np_array>=100)))
+            #print(np.any(np_array>=255))
+            np.savetxt(f,np_array)
+            f.close()
+            self.count = 1
+            
 
 def main(args = None):
         rclpy.init(args=args)
