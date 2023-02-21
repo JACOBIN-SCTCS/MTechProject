7df1ad7de86f9482519ba5a46abe5e0b82bbbe3d
Jacob <jacobja3@gmail.com>
2023-02-21 11:42:15 +0530

    Added to goal navigation feature offto adding path navigation

--- src/robot_controller/robot_controller/robot_h_signature.py
+++ src/robot_controller/robot_controller/robot_h_signature.py
@@ -5,6 +5,9 @@ from nav_msgs.msg import OccupancyGrid
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 import numpy as np
+from rclpy.action import ActionClient
+from nav2_msgs.action import NavigateToPose
+from action_msgs.msg import GoalStatus
 
 class TopologicalExplore(Node):
 
@@ -25,9 +28,46 @@ class TopologicalExplore(Node):
         )
         self.map_subscriber
 
+        self.exploration_client = ActionClient(self,NavigateToPose,'navigate_to_pose')
+
+
+    def goal_response_callback(self,future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Exploration goal rejected')
+            return
+        self.get_logger().info('Navigation goal rejected')
+        self._get_result_future  = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+    
+    def get_result_callback(self,future):
+        result = future.result().result
+        status = future.result().status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info('Arrived at destination')
+        else:
+            self.get_logger().info('Goal Failed')
+        
+    def send_goal(self):
+        self.exploration_client.wait_for_server()
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose.header.frame_id='map'
+        goal_msg.pose.pose.position.x = 0.0
+        goal_msg.pose.pose.position.y = -1.0
+        
+        self._send_goal_future = self.exploration_client.send_goal_async(goal_msg)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        rclpy.spin_until_future_complete(self,self._send_goal_future)
+        goal_handle = self._send_goal_future.result()
+        get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self,get_result_future)
+
+
+
     def map_callback(self,msg:OccupancyGrid):
-        #if self.navigating:
-        #    return
+        if not self.navigating:
+            self.send_goal()
+            self.navigating = True
         try:
             t  = self.tf_buffer.lookup_transform('robot_1/base_footprint','map',rclpy.time.Time())
             self.get_logger().info(str(t.transform.translation.x) + "," + str(t.transform.translation.y))
